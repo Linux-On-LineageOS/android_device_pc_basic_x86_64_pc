6dd16c649ec28498348a9d015e2cba292d4c0384
Archie Meng <archiemeng@protonmail.com>
2025-11-03 18:14:41 +0800

    Disable SELinux

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -81,3 +81,4 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/configs/fstab.basic_x86_64_pc
 # VINTF
 DEVICE_MANIFEST_FILE := \
     $(DEVICE_PATH)/configs/manifest.xml
+BOARD_BOOTCONFIG += androidboot.selinux=permissive
